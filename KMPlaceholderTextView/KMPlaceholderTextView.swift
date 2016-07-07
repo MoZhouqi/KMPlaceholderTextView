a97f40c46f0521caa2231fc0f6c1f593a72f9a18
Zhouqi Mo <mozhouqi@gmail.com>
2016-07-08 01:06:32 +0800

    Make placeholderLabel public

--- KMPlaceholderTextView/KMPlaceholderTextView.swift
+++ KMPlaceholderTextView/KMPlaceholderTextView.swift
@@ -29,7 +29,7 @@ public class KMPlaceholderTextView: UITextView {
     private struct Constants {
         static let defaultiOSPlaceholderColor = UIColor(red: 0.0, green: 0.0, blue: 0.0980392, alpha: 0.22)
     }
-    private let placeholderLabel: UILabel = UILabel()
+    public let placeholderLabel: UILabel = UILabel()
     
     private var placeholderLabelConstraints = [NSLayoutConstraint]()
     
